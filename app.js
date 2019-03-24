20b14234a0a6e93f4185d5373dcbed79072bb47f
Mr. Sundance <sundancekid1108@gmail.com>
2019-03-24 15:13:36 +0900

    1. search
    1. search

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -15,9 +17,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
-//app.use(routes.home, userRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
