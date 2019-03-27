ab2208240141859b21cad6b10239b046941aa327
Mr. Sundance <sundancekid1108@gmail.com>
2019-03-27 14:14:38 +0900

    webpack 설정 (진행)
    webpack 설정 (진행)

--- webpack.config.js
+++ webpack.config.js
@@ -20,29 +20,35 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 
 //Output => 생성된 번들 저장위치
 const OUTPUT_DIR = path.join(__dirname, "static");
-
 const config = {
-    entry: ENTRY_FILE,
+    entry: ["@babel/polyfill", ENTRY_FILE],
     mode: MODE,
     module: {
         rules: [{
-            test: /\.(scss)$/,
-            use: ExtractCSS.extract([{
-                    loader: "css-loader"
-                },
-                {
-                    loader: "postcss-loader",
-                    options: {
-                        plugin() {
-                            return [autoprefixer({ browsers: "cover 99.5%" })];
+                test: /\.(js)$/,
+                use: [{
+                    loader: "babel-loader"
+                }]
+            },
+            {
+                test: /\.(scss)$/,
+                use: ExtractCSS.extract([{
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins() {
+                                return [autoprefixer({ browsers: "cover 99.5%" })];
+                            }
                         }
+                    },
+                    {
+                        loader: "sass-loader"
                     }
-                },
-                {
-                    loader: "sass-loader"
-                }
-            ])
-        }]
+                ])
+            }
+        ]
     },
     output: {
         path: OUTPUT_DIR,
