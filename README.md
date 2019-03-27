53f5b6a4e5c368a8e053a7c8b1afb40405eec0b5
Mr. Sundance <sundancekid1108@gmail.com>
2019-03-27 13:41:54 +0900

    webpack설정
    webpack설정

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Youtube_Clone
 유튜브 따라 만들기
+
+4.1 #4.1 Styles with Webpack part One부터 시작
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/**
+ * webpack 설정파일..
+ * webpack => Module bundler(간단히 여러개의 파일을 하나로 묶어주는 Library)
+ * 장점
+ * - network 병목현상 해결
+ * - 모듈단위 개발 => 효율상승, 스코프 신경 안씀..
+ * 
+ * 
+ * loader => 스타일시트, 이미지 등을  webpack이 이해할 수 있는 js모듈로 변경해주는 역할
+ */
+
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+
+//Entry =>  종속성 그래프의 시작점
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+
+//Output => 생성된 번들 저장위치
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+    entry: ENTRY_FILE,
+    mode: MODE,
+    module: {
+        rules: [{
+            test: /\.(scss)$/,
+            use: ExtractCSS.extract([{
+                    loader: "css-loader"
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        plugin() {
+                            return [autoprefixer({ browsers: "cover 99.5%" })];
+                        }
+                    }
+                },
+                {
+                    loader: "sass-loader"
+                }
+            ])
+        }]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js"
+    },
+    plugins: [new ExtractCSS("styles.css")]
+};
+
+module.exports = config;
