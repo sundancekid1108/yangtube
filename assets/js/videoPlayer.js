57e43b2a7eb8b86dbbf1d506bea9c500b504045a
Mr. Sundance <sundancekid1108@gmail.com>
2019-04-08 13:26:55 +0900

    webcam record
    webcam record

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -5,7 +5,7 @@ const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
-
+const volumeRange = document.getElementById("jsVolume");
 
 //재생버튼
 function handlePlayClick() {
@@ -23,7 +23,9 @@ function handleVolumeClick() {
     if (videoPlayer.muted) {
         videoPlayer.muted = false;
         volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+        volumeRange.value = videoPlayer.volume;
     } else {
+        volumeRange.value = 0;
         videoPlayer.muted = true;
         volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
     }
@@ -82,7 +84,7 @@ const formatDate = seconds => {
 }
 
 function getCurrentTime() {
-    currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+    currentTime.innerHTML = formatDate(Math.floor(videoPlayer.currentTime));
 }
 
 function setTotalTime() {
@@ -91,12 +93,36 @@ function setTotalTime() {
     setInterval(getCurrentTime, 1000);
 }
 
+function handleEnded() {
+    videoPlayer.currentTime = 0;
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+}
+
+function handleDrag(event) {
+    const {
+        target: { value }
+    } = event;
+
+    videoPlayer.volume = value;
+    if (value >= 0.6) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else if (value >= 0.2) {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-down"></i>';
+    } else {
+        volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+    }
+
+}
+
 
 function init() {
+    videoPlayer.volume = 0.5;
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScrnBtn.addEventListener("click", goFullSscreen);
     videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+    videoPlayer.addEventListener("ended", handleEnded);
+    volumeRange.addEventListener("input", handleDrag);
 }
 
 
