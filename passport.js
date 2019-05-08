b9eb30f993ad58769b76e5b714fb13fea7a3bba8
Mr. Sundance <sundancekid1108@gmail.com>
2019-05-08 14:37:07 +0900

    에러 수정
    페이스북 연동 관련

--- passport.js
+++ passport.js
@@ -16,13 +16,12 @@ passport.use(
     }, githubLoginCallback)
 );
 
+//추후 수정할 것..(callbackURL부분)
 passport.use(
     new FacebookStrategy({
             clientID: process.env.FB_ID,
             clientSecret: process.env.FB_SECRET,
-            callbackURL: `https://afraid-baboon-46.localtunnel.me${
-        routes.facebookCallback
-      }`,
+            callbackURL: `http://localhost:4000${routes.facebookCallback}`,
             profileFields: ["id", "displayName", "photos", "email"],
             scope: ["public_profile", "email"]
         },
